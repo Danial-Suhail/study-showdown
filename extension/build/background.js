ff6a127a262c87aa49fdeae1e1eba75a0d0dfbe5
Mohammad Alwattar <watermohammad06@gmail.com>
2024-08-03 12:47:47 -0400

    pushing background and tabtracker

---
+++ extension/build/background.js
@@ -0,0 +1,18 @@
+/*global chrome*/
+function handleTabChange() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (tabs.length === 0) return; // No active tab found
+    var activeTab = tabs[0];
+    console.log(activeTab);
+  });
+}
+
+// Listen for when the active tab changes
+chrome.tabs.onActivated.addListener(handleTabChange);
+
+// Listen for when a tab's URL is updated
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.url) {
+    handleTabChange(); // Only log when the URL changes
+  }
+});
