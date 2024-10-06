795130b00e9a993dece6c2dd2a661e6c948f9732
firasaj <firas.aj@icloud.com>
2024-10-06 17:38:49 -0400

    TO-DO: FIX NAVBAR BEING PINNED TO WALLPAPER

--- website/study-showdown-website/app/page.tsx
+++ website/study-showdown-website/app/page.tsx
@@ -12,21 +12,25 @@ export default function Home() {
   }
 
   return (
-    <div>
-    <div className="bg-cover dark-overlay bg-[url('../public/images/study-showdown-wallpaper(3).png')]">
+    <div className="bg-cover dark-overlay bg-[url('../public/images/study-showdown-wallpaper(4).png')]">
+    <div >
       <Navbar />
-      <div className="max-w-[70%] w-full h-screen mx-auto text-center flex flex-col ">
-        <h1 className='mt-48 text-8xl font-bold tracking-wider font-brkreg text-stylized1'>Study Showdown</h1>
-          
-        <div className='mt-4 font-medium tracking-wider text-4xl'>
-          <p className="font-brkreg">helping you</p>
-          <ReactTyped className="pl-2 font-brkreg" 
-                      strings={['get motivated', 'study better', 'build discipline', 'make studying fun again']} 
-                      typeSpeed={75} backSpeed={75} loop />
-          
-        </div>
-          <div className="mt-32 my-auto">
-          <Button onClick={handleSignIn} buttonName={"Get Started"} />
+      {/* fix why the background is pinned to the Navbar */}
+      <div className="max-w-[70%] w-full h-[calc(100vh-6rem)] mx-auto text-center flex flex-col"> 
+        
+        <div className='flex-col justify-between'>
+          <h1 className='text-8xl font-bold tracking-wider font-brkreg text-stylized1'>Study Showdown</h1>
+            
+          <div className='mt-4 font-medium tracking-wider text-4xl'>
+            <p className="font-brkreg">helping you</p>
+            <ReactTyped className="pl-2 font-brkreg" 
+                        strings={['get motivated', 'study better', 'build discipline', 'make studying fun again']} 
+                        typeSpeed={75} backSpeed={75} loop />
+            
+          </div>
+            <div className="mt-32 my-auto">
+            <Button onClick={handleSignIn} buttonName={"Get Started"} />
+          </div>
         </div>
       </div>
     </div>     
